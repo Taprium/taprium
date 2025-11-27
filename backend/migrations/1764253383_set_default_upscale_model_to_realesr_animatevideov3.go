96d8d18a8805b1e2f82f935b64e4d018c0687aba
fengjiongmax <fengjiongmax@gmail.com>
2025-11-27 14:26:20 +0000

    update default model name, update models selection names

---
+++ backend/migrations/1764253383_set_default_upscale_model_to_realesr_animatevideov3.go
@@ -0,0 +1,29 @@
+package migrations
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase/core"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(app core.App) error {
+		generationSettingsRecord, _ := app.FindFirstRecordByFilter("settings", "")
+
+		if generationSettingsRecord == nil {
+			return nil
+		}
+
+		log.Println("Generating default settings")
+
+		generationSettingsRecord.Set("upscale_model", "realesr-animevideov3")
+		app.Save(generationSettingsRecord)
+
+		return nil
+	}, func(app core.App) error {
+		// add down queries...
+
+		return nil
+	})
+}
